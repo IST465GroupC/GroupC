92572818d0f8991e543238bee503320ee3b78f32
IST465GroupC <eliaschbeir11@gmail.com>
2015-11-09 16:45:03 -0500

    1

--- Applications.php
+++ Applications.php
@@ -8,14 +8,45 @@
     <body>
         <p>
        <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 //$servername = "us-cdbr-azure-east-a.cloudapp.net";
 //$username = "bfeb7e31e89ee0";
 //$password = "85679ca6";
 //$dbName = "as_93ca2a486ed92ff";
 
+$first = $_POST['first_name'];
+$last = $_POST['last_name'];
+$email = $_POST['email'];
+$tele = $_POST['telephone'];
+$comments = $_POST['comments'];
+
+if (!isset($_POST['first'])) {
+    
+}
+
 // Create connection
-$conn = new mysql_connect('us-cdbr-azure-east-a.cloudapp.net', 'bfeb7e31e89ee0', '85679ca6');
-echo($conn);
+$sql = new PDO('mysql:host=us-cdbr-azure-east-a.cloudapp.net;dbname=as_93ca2a486ed92ff;charset=utf8', 'bfeb7e31e89ee0', '85679ca6');
+$sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$sql->setAttribute(PDO::ATTR_PERSISTENT, false);
+
+
+echo "<br/>";
+
+$query = $sql->prepare('INSERT into applications (First, Last, Email, Telephone, Comments) VALUES (?, ?, ?, ?, ?)');
+$query->execute(array($first, $last, $email, $tele, $comments));
+
+
+$query = $sql->prepare('SELECT * FROM applications');
+$query->execute();
+
+$result = $query->fetchAll();
+
+print_r($result);
+
+//$conn = new mysqli_connect('us-cdbr-azure-east-a.cloudapp.net', 'bfeb7e31e89ee0', '85679ca6');
+//echo($conn);
 // Check connection
 //if (!$conn) {
 // echo('Connection failed: ' . mysql_error());
@@ -35,7 +66,7 @@ echo($conn);
     echo("Error: " . $sql . "<br>" . mysql_error($conn)) ;
 }
 */
-mysql_close($conn);
+//mysql_close($conn);
 ?>
             </p>
     </body>
