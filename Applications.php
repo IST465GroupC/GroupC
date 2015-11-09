fcef7ce11e54e4451d1e22602b47c5e42f37255c
IST465GroupC <eliaschbeir11@gmail.com>
2015-11-09 15:30:21 -0500

    0

--- Applications.php
+++ Applications.php
@@ -15,19 +15,20 @@
 
 // Create connection
 $conn = new mysql_connect('us-cdbr-azure-east-a.cloudapp.net', 'bfeb7e31e89ee0', '85679ca6');
+echo($conn);
 // Check connection
-if (!$conn) {
- echo('Connection failed: ' . mysql_error());
-}
- echo('Connected successfully'); 
+//if (!$conn) {
+// echo('Connection failed: ' . mysql_error());
+//}
+// echo('Connected successfully'); 
 
-$first = $_POST['first_name'];
-$last = $_POST['last_name'];
-$email = $_POST['email'];
-$tele = $_POST['telephone'];
-$comments = $_POST['comments'];
+//$first = $_POST['first_name'];
+//$last = $_POST['last_name'];
+//$email = $_POST['email'];
+//$tele = $_POST['telephone'];
+//$comments = $_POST['comments'];
 
-$sql = "INSERT into as_93ca2a486ed92ff.applications (First, Last, Email, Telephone, Comments) VALUES ('$first', '$last', '$email', '$tele', '$comments');";
+//$sql = "INSERT into as_93ca2a486ed92ff.applications (First, Last, Email, Telephone, Comments) VALUES ('$first', '$last', '$email', '$tele', '$comments');";
 /*if (mysql_query($sql, $conn)) {
     echo("New record created successfully") ;
 } else {
